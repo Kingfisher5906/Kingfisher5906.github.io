a893a3dca1355d62df3a74dfb0c29d294fe9d49f
Kingfisher5906 <89612814+Kingfisher5906@users.noreply.github.com>
2023-11-28 09:37:30 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,31 +10,18 @@ app.appendChild(logo)
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.open('GET', 'https://www.googleapis.com/books/v1/users/userId/bookshelves/shelf', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach((movie) => {
+  
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
       const h1 = document.createElement('h1')
-      h1.textContent = movie.title
+      h1.textContent = title
 
-      const p = document.createElement('p')
-      movie.description = movie.description.substring(0, 300)
-      p.textContent = `${movie.description}...`
-
-      container.appendChild(card)
-      card.appendChild(h1)
-      card.appendChild(p)
-    })
-  } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Gah, it's not working!`
-    app.appendChild(errorMessage)
-  }
+      
 }
 
 request.send()
