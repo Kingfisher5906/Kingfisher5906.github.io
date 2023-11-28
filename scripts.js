e32c700632daa57df8ccbf23c919bfb6ab3f8941
Kingfisher5906 <89612814+Kingfisher5906@users.noreply.github.com>
2023-11-28 09:38:44 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo)
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://www.googleapis.com/books/v1/users/userId/bookshelves/shelf', true)
+request.open('GET', 'https://www.googleapis.com/books/v1/users/userId/bookshelves/1', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
